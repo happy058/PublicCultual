3ccdd0a490edfa681a8f4f86a3373bdc2089ac5d
happy058 <2596828570@qq.com>
2018-01-24 10:06:33 +0800

    modify stringTest1

--- stringTest1.py
+++ stringTest1.py
@@ -4,16 +4,16 @@ class StringTestCase(unittest.TestCase):
     def setUp(self):
         self.test_string="This is a string"
 
-    def testReverse(self):
-       self.assertEqual("gnirts a si sihT",self.test_string[::-1])
+    #def testReverse(self):
+       #self.assertEqual("gnirts a si sihT",self.test_string[::-1])
 
     def testSplit(self):
         expected = ["This","is","a","string"]
         self.assertEqual(expected,self.test_string.split(" "))
         self.assertEqual(expected, self.test_string.split())
 
-    def testLower(self):
-        self.assertEqual("this is a string",self.test_string.lower())
+    #def testLower(self):
+        #self.assertEqual("this is a string",self.test_string.lower())
 
     def testUpper(self):
         self.assertEqual("THIS IS A STRING",self.test_string.upper())
