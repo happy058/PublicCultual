0a0fb7d62e07fae1a1aa101b4e568e42f3b01661
happy058 <2596828570@qq.com>
2018-01-24 13:32:45 +0800

    modify stringTest1

--- stringTest1.py
+++ stringTest1.py
@@ -18,9 +18,9 @@ class StringTestCase(unittest.TestCase):
     #def testUpper(self):
         #self.assertEqual("THIS IS A STRING",self.test_string.upper())
 
-    #def testRstrip(self):
-        #string = "This is a string        "
-        #self.assertEqual(self.test_string,string.rstrip())
+    def testRstrip(self):
+        string = "This is a string        "
+        self.assertEqual(self.test_string,string.rstrip())
 
     def testLstrip(self):
         string = "      This is a string"
